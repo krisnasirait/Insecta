5e7c1c659c0c8d0aeb3faf8754659be3997a35a2
krisnasirait <krisnasirait48@gmail.com>
2023-05-20 19:57:46 +0700

    added splash screen

---
+++ app/src/main/java/com/insecta/edu/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.insecta.edu
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.insecta.edu.databinding.ActivityMainBinding
+import com.insecta.edu.databinding.ActivitySplashBinding
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+    private val splashTime = 4000L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+            finish()
+        }, splashTime)
+
+        setContentView(binding.root)
+    }
+}
